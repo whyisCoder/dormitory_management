537869d8219e1064a3d48bf19a882f708021f05b
ImCoder <1534249353@qq.com>
2020-03-06 16:30:16 +0800

    登陆组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from "@/components/Login/login.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: '',
-      component: 
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
